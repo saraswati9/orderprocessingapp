c8c91839ca9e05ff6a92b3bf70bafa0f936f4279
saraswati9 <saraswati.pmk@gmail.com>
2021-11-25 01:43:06 +0530

    Adding project to git

---
+++ orderprocessingapp/src/main/java/com/example/orderprocessingapp/OrderprocessingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderprocessingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderprocessingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderprocessingappApplication.class, args);
+	}
+
+}
---
+++ orderprocessingapp/src/test/java/com/example/orderprocessingapp/OrderprocessingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderprocessingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderprocessingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
